f631f619488ad74eccd5d321c41358b8f3bafbb9
John Speck <speckworks@gmail.com>
2019-10-28 13:18:26 -0400

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ document.querySelector("#main").remove()
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "John is the champion"
+newHeader.innerHTML = "John is the champion."
 newHeader.className = "victory";
+document.body.append(newHeader)
