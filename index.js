55c82672cff607c5dba585774a3d85287a02bc25
Theresa <cyrustheresa@gmail.com>
2019-11-18 17:37:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+// Selects the main element with an id ='main' and removes it.
+// Step 1)    ✓ no longer has DOM node 'main#main'
+document.querySelector('main#main').remove();
+
+//Step 2)     ✓ has a 'newHeader' variable that points to node 'h1#victory'
+/*When the test states points to node it means creating/assigning */
+let newHeader = document.createElement('h1');
+
+//Step 3)      ✓ has a 'newHeader' variable that points to node 'h1#victory'
+newHeader.id = "victory"
+// Step 4)     ✓ has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "Theresa is the champion"
